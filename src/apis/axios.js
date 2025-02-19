7a9a9c0a0434f4a2148b653e2c0998651fbf9162
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-02-19 22:44:18 +0530

    Implemented react-query

---
+++ src/apis/axios.js
@@ -0,0 +1,61 @@
+import axios from "axios";
+import { t } from "i18next";
+import { keysToCamelCase, serializeKeysToSnakeCase } from "neetocist";
+import { Toastr } from "neetoui";
+import { evolve } from "ramda";
+import { OMDB_API_KEY, OMDB_API_URL } from "src/constants";
+
+const requestInterceptors = () => {
+  axios.interceptors.request.use(
+    evolve({ data: serializeKeysToSnakeCase, params: serializeKeysToSnakeCase })
+  );
+};
+
+const shouldShowToastr = response =>
+  typeof response === "object" && response?.noticeCode;
+
+const showSuccessToastr = response => {
+  if (shouldShowToastr(response.data)) Toastr.success(response.data);
+};
+
+const showErrorToastr = error => {
+  if (error.message === t("error.networkError")) {
+    Toastr.error(t("error.noInternetConnection"));
+  } else if (error.response?.status !== 404) {
+    Toastr.error(error);
+  }
+};
+
+const transformResponseKeysToCamelCase = response => {
+  if (response.data) response.data = keysToCamelCase(response.data);
+};
+
+const responseInterceptors = () => {
+  axios.interceptors.response.use(
+    response => {
+      transformResponseKeysToCamelCase(response);
+      showSuccessToastr(response);
+
+      return response.data;
+    },
+    error => {
+      showErrorToastr(error);
+
+      return Promise.reject(error);
+    }
+  );
+};
+
+const setHttpHeaders = () => {
+  axios.defaults.headers = {
+    Accept: "application/json",
+    "Content-Type": "application/json",
+  };
+};
+
+export default function initializeAxios() {
+  axios.defaults.baseURL = `${OMDB_API_URL}${OMDB_API_KEY}`;
+  setHttpHeaders();
+  responseInterceptors();
+  requestInterceptors();
+}
