7c907e9afe461974807caf161082c83d649cb9b8
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-02-20 10:58:07 +0530

    added translation for empty history

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -1,6 +1,7 @@
 import React, { useRef, useEffect } from "react";
 
 import classNames from "classnames";
+import { isEmpty } from "ramda";
 import { useTranslation, Trans } from "react-i18next";
 import useMoviesStore from "stores/useMoviesStore";
 
@@ -26,24 +27,30 @@ const ViewHistory = () => {
         {t("sectionHeadings.historySection")}
       </h2>
       <div className="max-h-[70vh] space-y-2 overflow-y-auto" ref={historyRef}>
-        {movies.map((movie, index) => (
-          <div
-            key={`${movie.Title}-${index}`}
-            ref={el => (itemRefs.current[movie.Title] = el)}
-            className={classNames(
-              "rounded-lg p-3 text-center transition-colors",
-              movie === selectedMovie
-                ? "bg-blue-600 text-white"
-                : "bg-blue-100 text-black"
-            )}
-          >
-            <Trans
-              components={{ span: <span /> }}
-              i18nKey="historyMovieNames"
-              values={{ Title: movie.Title }}
-            />
+        {isEmpty(movies) ? (
+          <div className="my-96 flex h-full justify-center text-center text-gray-500">
+            {t("displayMessages.emptyHistory")}
           </div>
-        ))}
+        ) : (
+          movies.map((movie, index) => (
+            <div
+              key={`${movie.Title}-${index}`}
+              ref={el => (itemRefs.current[movie.Title] = el)}
+              className={classNames(
+                "rounded-lg p-3 text-center transition-colors",
+                movie === selectedMovie
+                  ? "bg-blue-600 text-white"
+                  : "bg-blue-100 text-black"
+              )}
+            >
+              <Trans
+                components={{ span: <span /> }}
+                i18nKey="historyMovieNames"
+                values={{ Title: movie.Title }}
+              />
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
