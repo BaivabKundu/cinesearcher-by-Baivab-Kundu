ad905110bda4d04a140463788eae8582a69b2f36
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-02-24 11:59:49 +0530

    Used neetocist functions

--- src/stores/useMoviesStore.js
+++ src/stores/useMoviesStore.js
@@ -1,17 +1,31 @@
+import { removeBy } from "neetocist";
 import { uniqBy, pipe, append } from "ramda";
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
-const useMoviesStore = create(set => ({
-  movies: [],
-  lastSelectedMovie: null,
-  setMovies: movie =>
-    set(state => ({
-      movies: pipe(
-        append(movie),
-        uniqBy(m => m.imdbID)
-      )(state.movies),
-    })),
-  setLastSelectedMovie: movie => set({ lastSelectedMovie: movie }),
-}));
+const useMoviesStore = create(
+  persist(
+    set => ({
+      movies: [],
+      lastSelectedMovie: null,
+      addMovieToHistory: movie =>
+        set(state => ({
+          movies: pipe(
+            append(movie),
+            uniqBy(m => m.imdbID)
+          )(state.movies),
+        })),
+      setLastSelectedMovie: movie => set({ lastSelectedMovie: movie }),
+      removeMovieFromHistory: movieId =>
+        set(state => ({
+          movies: removeBy({ imdbID: movieId }, state.movies),
+        })),
+      clearAllMoviesFromHistory: () => set({ movies: [] }),
+    }),
+    {
+      name: "movies",
+    }
+  )
+);
 
 export default useMoviesStore;
