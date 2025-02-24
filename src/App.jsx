ca11397a30096bf0d417ca625ff426862609880c
Baivab Kundu <baivab.kundu@bigbinary.com>
2025-02-24 10:55:07 +0530

    Added routing functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,22 @@
+import Header from "components/commons/Header";
+import PageNotFound from "components/commons/PageNotFound";
+import Favourites from "components/Favourites";
+import Home from "components/Home";
 import { Redirect, Route, Switch } from "react-router-dom";
 
 import "./App.css";
-import PageNotFound from "./components/commons/PageNotFound";
-import Home from "./components/Home";
 import routes from "./routes";
 
 const App = () => (
-  <Switch>
-    <Route exact component={Home} path={`${routes.movies.index}/:search?`} />
-    <Redirect exact path={routes.root} to={routes.movies.index} />
-    <Route component={PageNotFound} path="*" />
-  </Switch>
+  <>
+    <Header />
+    <Switch>
+      <Route exact component={Home} path={routes.movies} />
+      <Route exact component={Favourites} path={routes.favourites} />
+      <Redirect exact from={routes.root} to={routes.movies} />
+      <Route component={PageNotFound} path="*" />
+    </Switch>
+  </>
 );
 
 export default App;
